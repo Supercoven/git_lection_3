17100c386527d962ec52f70603dfc9e3d58475ce
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-14 17:49:52 +0500

    added type 04

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -26,4 +26,31 @@ void Method21(string msg, int count)
 }
 
 // Method21(msg: "Текст сообщения", count: 4);
-Method21(count: 4, msg: "TEXT");
+// Method21(count: 4, msg: "TEXT");
+
+// 3 группа методов
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+//int year = Method3();
+//Console.WriteLine(year);
+
+// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
