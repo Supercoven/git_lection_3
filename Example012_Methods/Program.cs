7a59618b17c4b3dbcd45582c660fc9a33d6f7c9d
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-14 18:13:14 +0500

    cycle within cycle

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -40,17 +40,41 @@ int Method3()
 
 // Вид 4
 
+//string Method4(int count, string text)
+//{
+//      int i = 0;
+//    string result = String.Empty;
+//
+//    while (i < count)
+//    {
+//        result = result + text;
+//        i++;
+//    }
+//    return result;
+//}
+//string res = Method4(10, "asdf");
+//Console.WriteLine(res);
+
+// Цикл for
+
 string Method4(int count, string text)
 {
-    int i = 0;
-    string result = String.Empty;
-
-    while (i < count)
+     string result = String.Empty;
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        result = result + text;       
     }
     return result;
 }
 string res = Method4(10, "asdf");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+//Вывод таблицы умножения на экран
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++) //тело метода
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}"); 
+    }
+    Console.WriteLine();
+}
